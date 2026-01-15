f2e6cd9dcc67b95f5cdd1152eb046a967b7b72d3
Shakib Khan <leadwithshakib@gmail.com>
2026-01-15 16:31:34 +0600

    feat: add favicon metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,33 @@ import { Navbar } from "@/components/navbar";
 export const metadata: Metadata = {
   title: "AXIS | Advanced Graphing & Scientific Calculator",
   description: "Precision meets elegance. AXIS is a powerful suite of mathematical tools for students, engineers, and researchers.",
+   icons: {
+    icon: [
+      {
+        url: "/favicon_io/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon_io/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      { url: "/favicon_io/favicon.ico", sizes: "any", type: "image/x-icon" },
+      {
+        url: "/favicon_io/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon_io/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: "/favicon_io/apple-touch-icon.png",
+  },
+  manifest: "/favicon_io/site.webmanifest",
 };
 
 export default function RootLayout({
